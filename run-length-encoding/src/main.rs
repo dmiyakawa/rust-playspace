becec67aad21cf797c6ee26327cfc1368f81a30b
Daisuke Miyakawa <2149473+dmiyakawa@users.noreply.github.com>
2022-02-23 09:23:06 +0900

    Add decode() to run-length-encoding

--- run-length-encoding/src/main.rs
+++ run-length-encoding/src/main.rs
@@ -1,7 +1,4 @@
 fn encode(decoded: &str) -> Result<String, &str> {
-    if decoded.len() == 0 {
-        return Err("Empty string given");
-    }
     let mut prev_ch_option: Option<char> = None;
     let mut ch_count: u32 = 0;
     let mut encoded: String = String::new();
@@ -38,16 +35,54 @@ fn encode(decoded: &str) -> Result<String, &str> {
 
 #[test]
 fn test_encode() {
-    assert!(encode("").is_err());
+    assert_eq!(Ok(String::from("")), encode(""));
     assert_eq!(Ok(String::from("a5b2c3a2")), encode("aaaaabbcccaa"));
 }
 
-fn main() {
-    let encoded = match encode("abcde") {
-        Ok(value) => value,
-        Err(msg) => {
-            panic!("Error: {:?}", msg);
+fn decode(encoded: &str) -> Result<String, &str> {
+    let mut prev_ch_option: Option<char> = None;
+    let mut ch_count: u32 = 0;
+    let mut decoded: String = String::new();
+    for cur_ch in encoded.chars() {
+        match prev_ch_option {
+            None => {
+                prev_ch_option = Some(cur_ch);
+            }
+            Some(prev_ch) => {
+                if cur_ch.is_digit(10) {
+                    ch_count = ch_count * 10 + cur_ch.to_digit(10).unwrap();
+                } else {
+                    decoded.push_str(
+                        &std::iter::repeat(prev_ch)
+                            .take(ch_count as usize)
+                            .collect::<String>(),
+                    );
+                    prev_ch_option = Some(cur_ch);
+                    ch_count = 0;
+                }
+            }
         }
-    };
-    println!("{}", encoded);
+    }
+    match prev_ch_option {
+        Some(prev_ch) => {
+            decoded.push_str(
+                &std::iter::repeat(prev_ch)
+                    .take(ch_count as usize)
+                    .collect::<String>(),
+            );
+        }
+        None => {}
+    }
+    Ok(decoded)
+}
+
+#[test]
+fn test_decode() {
+    assert_eq!(Ok(String::from("")), decode(""));
+    assert_eq!(Ok(String::from("aaaaabbccca")), decode("a5b2c3a1"));
+}
+
+fn main() {
+    println!("{}", encode("abcde").unwrap());
+    println!("{}", decode("a1b1c2d3e4").unwrap());
 }
